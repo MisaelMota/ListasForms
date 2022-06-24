e0aa34e4ce7f3eda49a36756e2b60673b9d577ee
MisaelMota <87102180+MisaelMota@users.noreply.github.com>
2022-06-23 23:12:34 -0400

    Add project files.

---
+++ ListasForms/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace ListasForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this._Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Position = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Salary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Afp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Sfs = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._TotalDiscount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._NetIcome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this._Id,
+            this._Name,
+            this._Position,
+            this._Salary,
+            this._Afp,
+            this._Sfs,
+            this._TotalDiscount,
+            this._NetIcome});
+            this.dataGridView1.Location = new System.Drawing.Point(217, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(611, 425);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // _Id
+            // 
+            this._Id.DataPropertyName = "Id";
+            this._Id.HeaderText = "Id";
+            this._Id.Name = "_Id";
+            // 
+            // _Name
+            // 
+            this._Name.DataPropertyName = "Name";
+            this._Name.HeaderText = "Name";
+            this._Name.Name = "_Name";
+            // 
+            // _Position
+            // 
+            this._Position.DataPropertyName = "Position";
+            this._Position.HeaderText = "Position";
+            this._Position.Name = "_Position";
+            // 
+            // _Salary
+            // 
+            this._Salary.DataPropertyName = "Salary";
+            this._Salary.HeaderText = "Salary";
+            this._Salary.Name = "_Salary";
+            // 
+            // _Afp
+            // 
+            this._Afp.DataPropertyName = "Afp";
+            this._Afp.HeaderText = "Afp";
+            this._Afp.Name = "_Afp";
+            // 
+            // _Sfs
+            // 
+            this._Sfs.DataPropertyName = "Sfs";
+            this._Sfs.HeaderText = "Sfs";
+            this._Sfs.Name = "_Sfs";
+            // 
+            // _TotalDiscount
+            // 
+            this._TotalDiscount.DataPropertyName = "TotalDiscount";
+            this._TotalDiscount.HeaderText = "Total Discount";
+            this._TotalDiscount.Name = "_TotalDiscount";
+            // 
+            // _NetIcome
+            // 
+            this._NetIcome.DataPropertyName = "NetIcome";
+            this._NetIcome.HeaderText = "Net Icome";
+            this._NetIcome.Name = "_NetIcome";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Salary";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Position";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "ID";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 304);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(71, 27);
+            this.btnSave.TabIndex = 23;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(12, 269);
+            this.txtSalary.Multiline = true;
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(124, 29);
+            this.txtSalary.TabIndex = 19;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(12, 213);
+            this.txtPosition.Multiline = true;
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(124, 29);
+            this.txtPosition.TabIndex = 18;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 151);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(124, 29);
+            this.txtName.TabIndex = 17;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 101);
+            this.txtId.Multiline = true;
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(124, 29);
+            this.txtId.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 27);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 30);
+            this.textBox1.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Write Emp ID:";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(12, 351);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(106, 40);
+            this.btnShow.TabIndex = 31;
+            this.btnShow.Text = "Show All Employees";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(12, 397);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(106, 40);
+            this.btnRemove.TabIndex = 32;
+            this.btnRemove.Text = "Remove a Employee";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 461);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Position;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Salary;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Afp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Sfs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _TotalDiscount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _NetIcome;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ ListasForms/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListasForms
+{
+    public partial class Form1 : Form
+    {
+
+        public List<Form1> LIEmployee = new List<Form1>();
+
+
+
+        public int Id { get; set; }
+        public string EName { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+        public double Afp { get; set; }
+        public double Sfs { get; set; }
+        public double TotalDiscount { get; set; }
+        public double NetIcome { get; set; }
+
+
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            btnSave.Click += BtnSave_Click;
+            button1.Click += Button1_Click;
+            btnShow.Click += BtnShow_Click;
+            btnRemove.Click += BtnRemove_Click;
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+            RemoveEmployee();
+        }
+
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            ShowEmployee();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ShowEmployeeById();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            AddEmployee();
+        }
+
+        public double AfpValue(double salary)
+        {
+            return salary * 0.0287;
+        }
+
+        public double SfaValue(double salary)
+        {
+            return salary * 0.0304;
+        }
+
+        public double TotalDiscountValue(double afp, double sfs)
+        {
+            return afp + sfs;
+        }
+
+        public double NetIcomeValue(double salary, double TotalDiscount)
+        {
+            return salary - TotalDiscount;
+        }
+
+        public void AddEmployee()
+        {          
+            
+
+                Form1 Em = new Form1();
+
+                Em.Id = Convert.ToInt32(txtId.Text);
+
+
+                Em.Name = txtName.Text;
+
+
+                Em.Position = txtPosition.Text;
+
+
+                Em.Salary = Convert.ToInt32(txtSalary.Text);
+
+                Em.Afp = AfpValue(Em.Salary);
+                Em.Sfs = SfaValue(Em.Salary);
+
+                Em.TotalDiscount = TotalDiscountValue(Em.Afp, Em.Sfs);
+
+                Em.NetIcome = NetIcomeValue(Em.Salary, Em.TotalDiscount);
+
+                LIEmployee.Add(Em);
+
+            dataGridView1.Rows.Clear();
+
+            foreach (var item in LIEmployee)
+            {
+                dataGridView1.Rows.Add(
+                  item.Id,
+                  item.Name,
+                  item.Position,
+                  item.Salary,
+                  item.Afp,
+                  item.Sfs,
+                  item.TotalDiscount,
+                  item.NetIcome);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        public void ShowEmployeeById()
+        {
+            
+            int Identify = Convert.ToInt32(textBox1.Text);
+
+            foreach (var item in LIEmployee)
+            {
+                if (Identify == item.Id)
+                {
+                    dataGridView1.Rows.Clear();
+
+                    dataGridView1.Rows.Add(
+                  item.Id,
+                  item.Name,
+                  item.Position,
+                  item.Salary,
+                  item.Afp,
+                  item.Sfs,
+                  item.TotalDiscount,
+                  item.NetIcome);
+                }
+            }
+
+        }
+
+        public void ShowEmployee()
+        {
+            dataGridView1.Rows.Clear();
+
+            foreach (var item in LIEmployee)
+            {
+                
+
+                dataGridView1.Rows.Add(
+              item.Id,
+              item.Name,
+              item.Position,
+              item.Salary,
+              item.Afp,
+              item.Sfs,
+              item.TotalDiscount,
+              item.NetIcome);
+            }
+
+        }
+
+        public void RemoveEmployee()
+        {
+            
+            int Identify = Convert.ToInt32(textBox1.Text);
+
+            foreach (var item in LIEmployee)
+            {
+                if (Identify == item.Id)
+                {
+                    LIEmployee.Remove(item);
+                    break;
+                }
+
+                
+                
+            }
+            ShowEmployee();
+
+        }
+
+      
+    }
+}
